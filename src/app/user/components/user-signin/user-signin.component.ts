de3d569278663e2127aab68197777904eda91d83
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-15 13:55:43 +0200

    UPDATE : Stack from form to service

--- src/app/user/components/user-signin/user-signin.component.ts
+++ src/app/user/components/user-signin/user-signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-signin',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSigninComponent implements OnInit {
 
-  constructor() { }
+  public signinForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.signinForm = this.formBuilder.group({
+      login: [
+        '', // Default value of login : ''
+        Validators.required
+      ],
+      pass: [
+        '',
+        Validators.required
+      ]
+    });
   }
 
+  public onSubmit(): void {
+    this.userService.signin(this.signinForm.value);
+  }
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { Logger } from 'src/app/core/helpers/logger';
+import { UserModel } from '../models/user-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private users: any[] = [
+    {
+      login: 'bond',
+      pass: '007'
+    },
+    {
+      login: 'casper',
+      pass: 'ghost'
+    }
+  ];
+
+  private user: UserModel | null = null;
 
   constructor() { }
+
+  /**
+   * 
+   * @param credentials From signinForm (login and password user entered)
+   * credentials => {login: 'toto', pass:'titi'}
+   */
+  public signin(credentials: any): void {
+    Logger.info(JSON.stringify(credentials));
+    // Yassine : approche 1 boucle et comparaison
+    for (const inUser of this.users) {
+      if (inUser.login === credentials.login && inUser.pass === credentials.pass) {
+        this.user = new UserModel();
+        this.user.setLogin(credentials.login);
+        this.user.setToken('Le Token qui sera renvoyé par le backend'); // fmjdkfjmqdskfjmqdskhmjqsei.dmqklsfdjqsmklnesmkjm.qmskjfmkned98956d
+        break;
+      }
+    }
+    // Soit this.user est une instance de UserModel soit il est null
+
+    // Approche old school
+    for (let i: number = 0; i < this.users.length; i++) {
+      const inUser: any = this.users[i];
+      if (inUser.login === credentials.login && inUser.pass === credentials.pass) {
+        this.user = new UserModel();
+        this.user.setLogin(credentials.login);
+        this.user.setToken('balamkjdfmqsk.xxxxxx.yyyyyyy');
+      }
+    }
+
+    // Approche JL : find
+    const foundUser: any = this.users.find(
+      (inUser: any) => inUser.login === credentials.login && inUser.pass === credentials.pass
+    );
+    if (foundUser) {
+      this.user = new UserModel();
+      this.user.setLogin(credentials.login);
+      this.user.setToken('yyyyyyy.xxxxxx.zzzzzz');
+    }
+  }
+
+  public signout(): void {
+    this.user = null;
+  }
+
+  /**
+   * 
+   * @returns Yes or No a user was authenticated
+   */
+  public isAuthenticated(): boolean {
+    return this.user !== null;
+  }
 }
