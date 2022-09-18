6cc35248492d38d4d88ada83e4957bc280eddb48
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-18 12:21:57 +0200

    UPDATE : match and serialize with backend

--- src/app/core/models/poe.ts
+++ src/app/core/models/poe.ts
@@ -1,10 +1,24 @@
+import { Expose, plainToInstance, Type } from "class-transformer";
+import 'reflect-metadata';
+import { Logger } from "../helpers/logger";
 export class POE {
-
+  @Expose()
   public id?: number;
-  public  title!: string;
-  public  createAt?: Date;
+  
+  @Expose()
+  public name: string = '';
+
+  @Expose()
+  @Type(() => Date)
+  public  beginDate?: Date;
+
+  @Expose()
+  @Type(() => Date)
   public  endDate?: Date;
 
-
+  public deserialize(plainPOE: any): POE {
+    const asClass: POE =  plainToInstance(POE, plainPOE, {excludeExtraneousValues: true});
+    return asClass;
+  }
 
 }
