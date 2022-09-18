b0d638092188da7d4c781310b8ea157eda40aca2
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-18 12:22:16 +0200

    ADD: sync validator date

---
+++ src/app/core/validators/date-less-than.ts
@@ -0,0 +1,23 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import * as moment from "moment";
+import { Logger } from "../helpers/logger";
+
+export class DateLessThan {
+    public static dateLessThan(
+        dateFieldName: string,
+        validationErrors: ValidationErrors
+    ): ValidatorFn {
+        return (form: AbstractControl): ValidationErrors | null => {
+            
+            const dateFieldControl: AbstractControl | null = form.get('birthDate');
+            
+            if (dateFieldControl && dateFieldControl.errors !== null) {
+                return null; // Already in error, so, let's continue
+            }
+            const value: moment.Moment = moment(dateFieldControl?.value);
+            const today: moment.Moment = moment();
+
+            return value.isAfter(today) ? validationErrors : null;
+        }
+    }
+}
