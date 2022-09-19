30bf7e2b09db6c387c53109fcd54e2da5b194bb7
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-19 14:04:35 +0200

    UPDATE : deserialization method

--- src/app/core/models/intern.ts
+++ src/app/core/models/intern.ts
@@ -1,13 +1,34 @@
+import { Expose, plainToInstance, Type } from "class-transformer";
+import 'reflect-metadata';
+
 export class Intern {
+  @Expose()
   public id?: number;
+
+  @Expose()
   public name: string;
+
+  @Expose()
   public firstname?: string;
+
+  @Expose()
   public phoneNumber?: string;
+
+  @Expose()
   public email?: string;
+
+  @Expose()
+  @Type(() => Date)
   public birthDate?: Date;
+
+  @Expose()
   public address?: string;
 
   public constructor() {
     this.name = '';
+  }
+
+  public deserialize(rawIntern: unknown): Intern {
+    return plainToInstance(Intern, rawIntern, {excludeExtraneousValues: true});
   }
 }
