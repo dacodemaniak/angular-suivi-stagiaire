1a0eba89978800f7097039c61e6a78249f5c8759
DaCodeManiaK <jla.webprojet@gmail.com>
2022-09-21 14:51:24 +0200

    UPDATE : return as earlier error

--- src/app/core/validators/date-validator.ts
+++ src/app/core/validators/date-validator.ts
@@ -3,6 +3,9 @@ import * as moment from "moment";
 
 export class DateValidator {
     public static dateNotLessThan(control: AbstractControl): {[key: string]: any} | null {
+        if (control.errors !== null) {
+            return null;
+        }
         const userEnteredDate: moment.Moment = moment(control.value);
         const today: moment.Moment = moment();
         return userEnteredDate.isSameOrAfter(today) ? {dateNotLessThan: true} : null;
