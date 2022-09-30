c34357f1a6a089e0b384f2dfcc1eff5b8baee6b5
monicapb <monica.pina.baracaldo@gmail.com>
2022-09-30 14:28:53 +0200

    UPDATE : Clean directive remove Map for Config

--- src/app/shared/directives/initials.directive.ts
+++ src/app/shared/directives/initials.directive.ts
@@ -18,44 +18,24 @@ export class InitialsDirective implements OnInit {
     color: '#000',
     verticalAlign: 'middle',
     textAlign: 'center',
-    trucmuche: 'Portnawak'
   };
 
   private nativeElement: HTMLElement;
 
   private stylesMap: Map<string, string> = new Map<string, string>();
 
-  
+
   constructor(
     private renderer: Renderer2,
     elementRef: ElementRef
-  ) { 
+  ) {
     this.nativeElement = elementRef.nativeElement;
-
-    // Sets styles as a Map
-    this.stylesMap
-      .set('height', '2em')
-      .set('width', '2em')
-      .set('line-height', '2em')
-      .set('background-color', 'rgba(127, 127, 127, .7')
-      .set('font-weight', 'bold')
-      .set('border-radius', '50%')
-      .set('vertical-align', 'middle')
-      .set('text-align', 'center');
   }
 
   public ngOnInit(): void {
-    //this.renderer.addClass(this.nativeElement, 'bubble');
-
-    /** Styles from a Map
-    this.stylesMap.forEach((value: string, key: string) => {
-      this.renderer.setStyle(this.nativeElement, key, value)
-    });
-    */
-
     // Styles from object
     const config: BubbleConfig = new BubbleConfig().deserialize(this.config);
-    
+
     for (const property in this.config) {
       this.renderer.setStyle(this.nativeElement, property, this.config[property]);
     }
@@ -63,6 +43,5 @@ export class InitialsDirective implements OnInit {
     const initials: string = this.intern!.firstname!.charAt(0) + this.intern!.name!.charAt(0);
 
     this.nativeElement.innerText = initials;
-    //this.renderer.createText(initials);
   }
 }
